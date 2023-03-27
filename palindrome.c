4aa36be7ee7c1724144918f6ca576a6fed30a05f
HarshikaBaranwal <127193065+HarshikaBaranwal@users.noreply.github.com>
2023-03-27 21:33:49 +0530

    palindrome

---
+++ palindrome.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int n ,s=0, a ,r;
+    printf ("enter the value of n:");
+    scanf ("%d",&n);
+    a=n;
+    while (n>0){
+        r=n%10;
+        s=r+(s*10);
+        n=n/10;
+        
+    }
+    if (s==a){
+            printf ("palindrome");
+        }
+        else{
+            printf ("not palindrome");
+        }
+}
