666c7675b4ce0fadc6bd753eb2ad68412ad0b01a
HarshikaBaranwal <127193065+HarshikaBaranwal@users.noreply.github.com>
2023-03-29 21:19:39 +0530

    even or odd

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a;
+    printf ("enter a num:");
+    scanf ("%d",&a);
+    if (a%2==0){
+        printf ("num is even");
+    }
+    else{
+        printf ("num is odd");
+    }
+
+}
